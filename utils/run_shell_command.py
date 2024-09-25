4d5d84da4c0fbf0f6ebaf2f7bc25a5a0a3816fc8
holy <752019594@qq.com>
2024-09-25 16:12:16 +0800

    update: auto run tb on first epoch

--- utils/run_shell_command.py
+++ utils/run_shell_command.py
@@ -3,6 +3,7 @@ import subprocess
 import socket
 import os
 import signal
+import time
 
 
 def run_shell_command(command):
@@ -10,28 +11,42 @@ def run_shell_command(command):
     运行shell命令
     """
     # print(f"正在运行shell命令: {command}")
-    results = subprocess.run(command, shell=True, capture_output=True, text=True)
+    results = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    output, err = results.communicate()
+    # 你可以添加以下代码来检测命令是否完成
+    while results.poll() is None:
+        time.sleep(1)
     if results.returncode == 0:
-        print(f"命令执行成功！{results.stdout}")
+        print(f"命令执行成功！{output}")
     else:
-        print(f"命令执行失败！, 请查看错误信息：{results.stderr}")
+        print(f"命令执行失败！, 请查看错误信息：{err}")
+    return results
+
+def kill_port(PORT):
+    print(f"正在清理端口占用...")
+    command = f"nohup kill -9 $(lsof -t -i:6007) > /dev/null 2>&1"
+    results = run_shell_command(command)
+    output, err = results.communicate()
+    if results.returncode == 0:
+        print(f"🗑️ {PORT} 端口已被清空, {output}")
+    else:
+        print(f"{err}")
     return results
 
 def start_tensorboard(log_path, PORT=6006, HOST='0.0.0.0', tb=False):
-    print(f"正在清理端口占用...")
-    command = f"nohup kill -9 $(lsof -t -i:{PORT}) > /dev/null 2>&1 &"
-    results = run_shell_command(command)
-    print(f"正在启动 tensorboard面板..., log_path: {log_path}")
+    kill_port(PORT)
+    print(f"😃 正在启动 tensorboard面板...\nlog_path: {log_path}")
     if tb:
         command = f"nohup tensorboard --logdir='{log_path}' --port={PORT} --host={HOST} > /dev/null 2>&1 &"
     else:
         command = f"nohup python3 -m tensorboard.main --logdir='{log_path}' --port={PORT} --host={HOST} > /dev/null 2>&1 &"
     results = run_shell_command(command)
+    output, err = results.communicate()
     if results.returncode == 0:
-        print(f"TensorBoard 启动成功！请通过 localhost:{PORT} 打开")
+        print(f"🆗TensorBoard 启动成功！请通过 localhost:{PORT} 打开, {output}")
     else:
-        print(f"TensorBoard 启动失败！, 请查看错误信息：{results.stderr}")
+        return f"❌TensorBoard 启动失败！ {err}"
 
 if __name__ == "__main__":
-    log_path = '/mnt/d/AI_Research/WS-HUB/WS-segBratsWorkflow/Helium-327-SegBrats/results/2024-09-24/【good】19-15-51/tensorBoard/UNet3D_braTS21_2024-09-24 19-15-47'
-    start_tensorboard(log_path, PORT=6008, HOST='0.0.0.0', tb=False)
+    log_path = '/root/data/workspace/Helium-327-SegBrats/results/2024-09-25/10-46-15/tensorBoard/UNet3D_braTS21_2024-09-25 10-46-11'
+    start_tensorboard(log_path, PORT=6007, HOST='0.0.0.0', tb=False)
