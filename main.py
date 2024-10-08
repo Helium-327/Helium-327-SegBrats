944d519b3d49b88ecea2d57d2f2cc77c77cfa37f
holy <752019594@qq.com>
2024-10-08 20:08:46 +0800

    更新train模块

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ from torch.optim import Adam, SGD, RMSprop, AdamW
 from torch.optim.lr_scheduler import ReduceLROnPlateau, CosineAnnealingLR
 from torch.amp import GradScaler
 
-from nets.unet3d.unet3d import *
+from nets.unet3d.unet3d_bn import UNet3D_BN
+from nets.unet3d.unet3d_ln import UNet3D_LN
+from nets.unet3d.unet3d_5x5 import UNet3D_BN_5x5
+# from nets.unet3d.uent3d_dilation import UNet3D_dilation
 from nets.model_weights_init import init_weights_light
 from loss_function import DiceLoss, CELoss, FocalLoss
 from utils.get_commits import *
@@ -60,8 +63,16 @@ def main(args):
 
     """------------------------------------- 模型实例化、初始化 --------------------------------------------"""
 
-    if args.model == 'UNet3D':
-        model = UNet3D(4, 4)
+    if args.model == 'unet3d_bn':
+        model = UNet3D_BN(4, 4)
+    elif args.model == 'unet3d_ln':
+        model = UNet3D_LN(4, 4)
+    elif args.model == 'unet3d_bn_5x5':
+        model = UNet3D_BN_5x5(4, 4)
+    # elif args.model == 'unet3d_dilation':
+    #     model = UNet3D_dilation(4, 4)
+    else:
+        raise ValueError(f"Invalid model name: {args.model}")
     
     init_weights_light(model)
     model.to(DEVICE)
@@ -108,16 +119,16 @@ def main(args):
                                                         tioRandomFlip3d(),                 # 随机翻转
                                                         # tioRandomElasticDeformation3d(),
                                                         # tioZNormalization(),               # 归一化
+                                                        Normalize(mean=(0.114, 0.090, 0.170, 0.096), std=(0.199, 0.151, 0.282, 0.174)),   # 标准化
                                                         tioRandomNoise3d(),
                                                         tioRandomGamma3d(),    
-                                                        Normalize(mean=(0.114, 0.090, 0.170, 0.096), std=(0.199, 0.151, 0.282, 0.174)),   # 标准化
                                                         # tioRandomAffine(),          # 随机旋转
                                       ]))
     
     TransMethods_val = data_transform(transform=Compose([RandomCrop3D(size=args.valCropSize),    # 随机裁剪
                                                         #  tioZNormalization(),               # 归一化
-                                                         tioRandomFlip3d(),   
                                                          Normalize(mean=(0.114, 0.090, 0.170, 0.096), std=(0.199, 0.151, 0.282, 0.174)),   # 标准化
+                                                         tioRandomFlip3d(),   
                                       ]))
     
     assert args.data_scale in ['debug', 'small', 'full'], "data_scale must be 'debug', 'small' or 'full'!"
@@ -183,6 +194,8 @@ def main(args):
     """------------------------------------- 调度器 --------------------------------------------"""
     if args.scheduler == 'ReduceLROnPlateau':
         scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=args.reduce_factor, patience=args.reduce_patience)
+        delattr(args,'cosine_T_max')
+        delattr(args,'cosine_min_lr')
     elif args.scheduler == 'CosineAnnealingLR':
         scheduler = CosineAnnealingLR(optimizer, T_max=args.cosine_T_max, eta_min=args.cosine_min_lr)
         delattr(args, 'reduce_patience')
@@ -246,12 +259,12 @@ if __name__ == "__main__":
     parser.add_argument("--results_root", type=str, default="./results", help="result path")
     parser.add_argument("--resume", type=str, default=None, help="resume training from checkpoint")
     
-    parser.add_argument("--model", type=str, default="UNet3D", help="models: ['UNet3D', 'UNet_3d_22M_32', 'UNet_3d_22M_64', 'UNet_3d_48M', 'UNet_3d_90M', 'UNet3d_bn_256', 'UNet3d_bn_512', 'UNet_3d_ln', 'UNet_3d_ln2']")
+    parser.add_argument("--model", type=str, default="unet3d_bn", help="models: ['unet3d_bn', 'unet3d_ln', 'unet3d_dilation', 'unet3d_bn_5x5']")
     parser.add_argument("--total_parms", type=int, default=None, required=False, help="total parameters")
-    parser.add_argument("--epochs", type=int, default=60, help="num_epochs")
-    parser.add_argument("--nw", type=int, default=8, help="num_workers")
+    parser.add_argument("--epochs", type=int, default=300, help="num_epochs")
+    parser.add_argument("--nw", type=int, default=4, help="num_workers")
     parser.add_argument("--bs", type=int, default=2, help="batch_size")
-    parser.add_argument("--early_stop_patience", type=int, default=20, help="early stop patience")
+    parser.add_argument("--early_stop_patience", type=int, default=50, help="early stop patience")
     
     parser.add_argument("--input_channels", type=int, default=4, help="input channels")
     parser.add_argument("--output_channels", type=int, default=4, help="output channels")
@@ -262,13 +275,13 @@ if __name__ == "__main__":
     # parser.add_argument("--loss_type", type=str, default="subarea_custom", help="loss type to grad")
     parser.add_argument("--save_max", type=int, default=5, help="ckpt max save number")
 
-    parser.add_argument("--optimizer", type=str, default="SGD", help="optimizers: ['AdamW', 'SGD', 'RMSprop']")
-    parser.add_argument("--lr", type=float, default=0.001, help="learning rate")
+    parser.add_argument("--optimizer", type=str, default="AdamW", help="optimizers: ['AdamW', 'SGD', 'RMSprop']")
+    parser.add_argument("--lr", type=float, default=3e-4, help="learning rate")
     parser.add_argument("--wd", type=float, default=1e-4, help="weight decay")
 
-    parser.add_argument("--scheduler", type=str, default=None, help="schedulers:['ReduceLROnPlateau', 'CosineAnnealingLR']")
-    parser.add_argument("--cosine_min_lr", type=float, default=1e-4, help="CosineAnnealingLR min lr")
-    parser.add_argument("--cosine_T_max", type=int, default=100, help="CosineAnnealingLR T max")
+    parser.add_argument("--scheduler", type=str, default='CosineAnnealingLR', help="schedulers:['ReduceLROnPlateau', 'CosineAnnealingLR']")
+    parser.add_argument("--cosine_min_lr", type=float, default=1e-8, help="CosineAnnealingLR min lr")
+    parser.add_argument("--cosine_T_max", type=int, default=300, help="CosineAnnealingLR T max")
     # parser.add_argument("--cosine_last_epoch", type=int, default=30, help="CosineAnnealingLR last epoch")
 
     parser.add_argument("--reduce_patience", type=int, default=3, help="ReduceLROnPlateau scheduler patience")
